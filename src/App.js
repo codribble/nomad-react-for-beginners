eb2a282cb2c87dae407278281f9dd45830b8ed9c
coDribble <sksrlatkddn@naver.com>
2023-10-26 18:05:17 +0900

    231026 #7 PRACTICE MOVIE APP
    7.0~7.1 To Do List

--- src/App.js
+++ src/App.js
@@ -1,49 +1,38 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword & counter' changes.");
-  }, [keyword, counter]);
-
-  const [showing, setShowing] = useState(false);
-  function Hello() {
-    useEffect(() => {
-      console.log("hi :)");
-      return () => console.log("bye :(");
-    }, []);
-
-    return <h1>Hello</h1>;
-  }
-
-  const onToggle = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search herr..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
 
-      {showing ? <Hello /> : null}
-      <button onClick={onToggle}>{showing ? "Hide" : "Show"}</button>
+      <hr />
+
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
